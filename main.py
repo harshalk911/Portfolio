5985f882eae0ccd02f999195945956fe1e1da402
Harshal Korade <korade54321@gmail.com>
2025-04-18 07:34:47 +0530

    added description, images and urls

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
-
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=600)
+    st.image("images/photo.png")
 
 with col2:
     st.title("Harshal Korade")
@@ -15,3 +15,23 @@ with col2:
     databases, and crafting dynamic user experiences with modern tools and frameworks.
     """
     st.info(content)
+
+st.subheader("Below you can find some apps I have built in Python. Feel free to contact me.")
+
+col3 , empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+data = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in data[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in data[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
