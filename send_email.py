8f80b81412076394c8a0611bff2b69fc963ccdb7
Harshal Korade <korade54321@gmail.com>
2025-04-19 09:03:19 +0530

    added dotenv library

--- send_email.py
+++ send_email.py
@@ -1,11 +1,16 @@
 import smtplib, ssl
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+env_pass = os.getenv("db_password")
+
 
 def email_sender(message):
     host = "smtp.gmail.com"
     port = 465
     username = "korade54321@gmail.com"
-    password = os.getenv("PASSWORD")
+    password = env_pass
     receiver = "korade54321@gmail.com"
 
     my_context = ssl.create_default_context()
